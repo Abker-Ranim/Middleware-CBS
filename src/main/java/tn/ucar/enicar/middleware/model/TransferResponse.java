5240092a9970f361885ad02f823bc13937637cdc
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-07-17 02:37:10 +0100

    Enhance TransferResponse class with MongoDB annotations and additional fields

--- src/main/java/tn/ucar/enicar/middleware/model/TransferResponse.java
+++ src/main/java/tn/ucar/enicar/middleware/model/TransferResponse.java
@@ -1,11 +1,15 @@
 package tn.ucar.enicar.middleware.model;
-
-
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "transfer_responses")
 @Data
 public class TransferResponse {
+    @Id
+    private String id;
     private String status;
     private double fromBalance;
     private double toBalance;
+    private String message;
 }
