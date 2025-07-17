1d747f88f703ee6d8bf1b1a051fae6034f6dc460
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-07-17 02:37:04 +0100

    Refactor TransactionHistory class to encapsulate transactions and add error handling

--- src/main/java/tn/ucar/enicar/middleware/model/TransactionHistory.java
+++ src/main/java/tn/ucar/enicar/middleware/model/TransactionHistory.java
@@ -6,12 +6,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class TransactionHistory extends ArrayList<Transaction> {
+public class TransactionHistory {
+    private List<Transaction> transactions;
+    private String error; // Pour stocker un message d'erreur si nécessaire
+
+    public TransactionHistory() {
+        this.transactions = new ArrayList<>();
+    }
+
+    public TransactionHistory(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
 }
 
-@Data
-class Transaction {
-    private String type;
-    private double amount;
-    private String date;
-}
