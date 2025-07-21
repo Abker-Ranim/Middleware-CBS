6f109b96b296a577ed27004c345d4b578a7637de
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-07-21 16:47:37 +0100

    Remove MongoDB document annotation and id field from TransferResponse class

--- src/main/java/tn/ucar/enicar/middleware/model/TransferResponse.java
+++ src/main/java/tn/ucar/enicar/middleware/model/TransferResponse.java
@@ -3,13 +3,10 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "transfer_responses")
 @Data
 public class TransferResponse {
-    @Id
-    private String id;
+
     private String status;
     private double fromBalance;
     private double toBalance;
-    private String message;
 }
