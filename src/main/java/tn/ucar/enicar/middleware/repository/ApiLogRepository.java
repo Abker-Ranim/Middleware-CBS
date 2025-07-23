4431ad4557f8e6af4397c57ab7d6784d3a664508
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-07-23 02:18:48 +0100

    delete ApiLog.java and ApiLogRepository.java

--- src/main/java/tn/ucar/enicar/middleware/repository/ApiLogRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.ucar.enicar.middleware.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import tn.ucar.enicar.middleware.model.ApiLog;
-
-public interface ApiLogRepository extends MongoRepository<ApiLog, String> {
-}
