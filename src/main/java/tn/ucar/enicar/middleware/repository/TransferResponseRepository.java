5d773936cad5fbb50744b2ef39b474e23dd97746
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-07-25 15:57:29 +0100

    delete models

--- src/main/java/tn/ucar/enicar/middleware/repository/TransferResponseRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.ucar.enicar.middleware.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import tn.ucar.enicar.middleware.model.TransferResponse;
-
-public interface TransferResponseRepository extends MongoRepository<TransferResponse, String> {
-}
